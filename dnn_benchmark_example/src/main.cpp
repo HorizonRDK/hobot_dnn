dd1c9989fc6d6e254baf54c7f92905a994c6092a
kao.zhu <kao.zhu@horizon.ai>
2022-06-10 13:40:31 +0800

    chore(license): [cr_id_skip] Add apache license no_3rd_skip
    Test Plan: verified
    
    Reviewers: kairui.wang
    
    Reviewed By: kairui.wang
    
    Differential Revision: https://cr.hobot.cc/D184557

--- dnn_benchmark_example/src/main.cpp
+++ dnn_benchmark_example/src/main.cpp
@@ -1,10 +1,16 @@
-// Copyright (c) 2021 Horizon Robotics.All Rights Reserved.
+// Copyright (c) 2022，Horizon Robotics.
 //
-// The material in this file is confidential and contains trade secrets
-// of Horizon Robotics Inc. This is proprietary information owned by
-// Horizon Robotics Inc. No part of this work may be disclosed,
-// reproduced, copied, transmitted, or used in any way for any purpose,
-// without the express written permission of Horizon Robotics Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <string>
 #include <memory>
--- dnn_node/include/util/threads/threadpool.h
+++ dnn_node/include/util/threads/threadpool.h
@@ -1,7 +1,17 @@
+// Copyright (c) 2022，Horizon Robotics.
 //
-// Created by chuanyi.yang@hobot.cc on 06/15/2018.
-// Copyright (c) 2018 horizon robotics. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef SRC_COMMON_THREADPOOL_H_
 #define SRC_COMMON_THREADPOOL_H_
 
